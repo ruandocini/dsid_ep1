2f060ad95daa043e3053ca54545a397e3a649a27
ruandocini <57142259+ruandocini@users.noreply.github.com>
2022-06-25 22:15:56 -0300

    Add files via upload
    Added first analysis

--- spark_analysis.py
+++ spark_analysis.py
@@ -1,6 +1,8 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+from pyspark.sql.functions import col, asc,desc, count, max, min
+
 
 conf = SparkConf()
 sc = SparkContext(conf=conf)
@@ -11,7 +13,22 @@ spark = SparkSession \
     .config("spark.some.config.option", "some-value") \
     .getOrCreate()
 
-collectionsEvents = spark.read.option("Header",True).csv("google-traces/collection_events/collection_events-000000000000.csv")
-instanceEvents = spark.read.option("Header",True).csv("google-traces/instance_events/instance_events-000000000000.csv")
+collectionsEvents = spark.read.option("Header",True).csv("google-traces/collection_events/collection_events-000000000000.csv", inferSchema=True)
+instanceEvents = spark.read.option("Header",True).csv("google-traces/instance_events/instance_events-000000000031.csv", inferSchema=True)
 
-collectionsEvents.show()
+##Primeiro irei responder quantos jobs são submetidos por horas no trace
+## Nesse caso seria necessário filtrar linhas que são pertencentes a alloc set
+## mas como não tenho o campo collection_type, vou considerar que todas as linhas são jobs
+jobsSubmitted = collectionsEvents.filter(collectionsEvents.type == 0)
+jobsSubmitted = jobsSubmitted.withColumn('time',col('time').cast("int"))
+jobsSubmitted = collectionsEvents.filter(collectionsEvents.time > 0)
+
+jobsCount = jobsSubmitted.select('time').rdd.count()
+microToHour = 3.6e+9
+
+traceEnd = jobsSubmitted.select('time').rdd.max()[0]
+traceStart = jobsSubmitted.select('time').rdd.min()[0]
+
+hoursElapsed = (traceEnd-traceStart)/microToHour
+
+jobsPerHour = jobsCount/hoursElapsed
